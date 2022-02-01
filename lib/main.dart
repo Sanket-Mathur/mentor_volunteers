e75f06fa9368e75fd0e2173bcdddfd21466712eb
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-02-01 22:49:35 +0530

    fix: MaterialApp structure and settig theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 
 import 'screens/Login.dart';
 
-void main() => runApp(const MaterialApp(
-      home: LandingPage(),
+// parent application widget
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Career 1-1',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      home: const LandingPage(),
     ));
 
 class LandingPage extends StatefulWidget {
@@ -17,11 +28,13 @@ class LandingPage extends StatefulWidget {
 }
 
 class LandingPageState extends State<LandingPage> {
+  // timer for 4 seconds before redirecting from splash screen
   startTime() async {
     var duration = const Duration(seconds: 4);
     return Timer(duration, route);
   }
 
+  // route to login page with page replacement
   route() {
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => const Login()));
@@ -35,44 +48,32 @@ class LandingPageState extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Career 1-1',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
-        textTheme: const TextTheme(
-          bodyText2: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 45),
-                child: Text(
-                  'Career 1-1',
-                  style: TextStyle(
-                    fontSize: 66,
-                    fontWeight: FontWeight.bold,
-                  ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 45),
+              child: Text(
+                'Career 1-1',
+                style: TextStyle(
+                  fontSize: 66,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40),
-                child: Text(
-                  'Provide the career guidance you were seeking for.',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                  textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                'Provide the career guidance you were seeking for.',
+                style: TextStyle(
+                  fontSize: 20,
                 ),
+                textAlign: TextAlign.center,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -5,33 +5,18 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Career 1-1',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
-        textTheme: const TextTheme(
-          bodyText2: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Career 1-1'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 40,
-                ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 40,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
